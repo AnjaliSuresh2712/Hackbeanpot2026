5e8beabdfcea5724cde286cab3a92062e7194af5
Edgar Castaneda <castaneda.e@northeastern.edu>
2026-02-14 17:14:24 -0500

    adding images

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-white">
+            <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-white to-yellow-50">
+                <div className="text-center">
+                    <img
+                        src="/images/tamagotchi.png"
+                        alt="Tamagotchi"
+                        className="mx-auto mb-8"
+                    />
+                </div>
+            </div>
+        </div>
+    );
+}
